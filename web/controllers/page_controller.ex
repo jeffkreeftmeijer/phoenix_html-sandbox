81008ab65dd486fece461088b1cb47b073efcb34
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-10-11 09:23:37 +0200

    Show parameters on "/form"

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,13 @@
 defmodule Sandbox.PageController do
   use Sandbox.Web, :controller
+  alias Sandbox.User
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
-  def form(conn, _params) do
-    render conn, "form.html"
+  def form(conn, params) do
+    changeset = User.changeset(%User{})
+    render conn, "form.html", changeset: changeset, params: params
   end
 end
